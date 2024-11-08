6cc8e8e8e960b2c44d9592299d44df83cc7ea396
yassienE4 <yassienebrahim@gmail.com>
2024-11-08 16:49:59 +0200

    .

--- main.cpp
+++ main.cpp
@@ -1,8 +1,20 @@
 #include <QApplication>
+#include <QSplashScreen>
+#include <QTimer>
+#include "mainmenu.h"
+
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    //loading screen implementation
+    QSplashScreen *loadscreen = new QSplashScreen;
+    loadscreen->setPixmap(QPixmap("/Users/yassien/Desktop/lecture_project/Lecture_Project/loadingscreen.png"));
+    loadscreen->show();
+
+    QTimer::singleShot(300,loadscreen,SLOT(close()));
+
 
     return a.exec();
 }
