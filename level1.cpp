751bdccbd1089e91e2a355c165afc1a5c639305a
talia-mebed <talia@aucegypt.edu>
2024-11-25 22:33:27 +0200

    I added several objects and edited moving enemy

--- level1.cpp
+++ level1.cpp
@@ -28,6 +28,10 @@ void level1::initialize()
     baselevel::addobstacle(obstacles[2]);
     obstacles.push_back(new obstacle(1500,420,300,100,":/images/woodenblock.png")); // block at 400,400 size 400/100
     baselevel::addobstacle(obstacles[3]);
+    obstacles.push_back(new obstacle(1700,350,200,50,":/images/woodenblock.png")); // block at 400,400 size 400/100
+    baselevel::addobstacle(obstacles[4]);
+    obstacles.push_back(new obstacle(2000,420,300,100,":/images/woodenblock.png")); // block at 400,400 size 400/100
+    baselevel::addobstacle(obstacles[5]);
 
 
     // adding coins(diamonds)
@@ -39,12 +43,14 @@ void level1::initialize()
     baselevel::adddiamond(diamond[2]);
     diamond.push_back(new diamonds(1700,300)); // diamond at 500,300
     baselevel::adddiamond(diamond[3]);
+    diamond.push_back(new diamonds(1800,220)); // diamond at 500,300
+    baselevel::adddiamond(diamond[3]);
 
 
     // adding enemies
     enemies.push_back(new moving_enemy(500,320,600,400)); // spawns enemy at pos 500(x),280(y) and moves from 400-600x
     baselevel::spawn_enemy(enemies[0]);
-    enemies.push_back(new moving_enemy(1900,420,600,400)); // spawns enemy at pos 500(x),280(y) and moves from 400-600x
+    enemies.push_back(new moving_enemy(1900,420,1000,800)); // spawns enemy at pos 500(x),280(y) and moves from 400-600x
     baselevel::spawn_enemy(enemies[1]);
 
 
