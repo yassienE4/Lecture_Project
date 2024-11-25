66110c77a143b919f29cd85cef904f0e43baaf7f
yassienE4 <yassienebrahim@gmail.com>
2024-11-25 16:13:13 +0200

    .

--- level1.cpp
+++ level1.cpp
@@ -25,20 +25,6 @@ void level1::initialize()
     obstacles.push_back(new obstacle(400,420,400,100,":/images/woodenblock.png")); // need to empty qlist after level1 ends
     baselevel::addobstacle(obstacles[1]);
 
-    /*      fix these
-    obstacle spike1(560,120,25,20,":/images/block.png");
-    obstacle wooden_block(300,200,200,50,":/images/block.png"); //want to place coins above it
-    obstacle block2(560,330,20,20,":/images/block.png");
-    obstacle spike2(560,400,25,20,":/images/block.png");
-    obstacle spike3(560,475,25,20,":/images/block.png");
-    obstacle block3(400,550,40,40,":/images/block.png");
-    obstacle wooden_block2(500,650,200,50,":/images/block.png"); // will have a spike at the end making the player jump
-    obstacle spike_on_block(500,700,25,20,":/images/block.png");
-    obstacle block4(400,900,20,20,":/images/block.png");
-    obstacle block5(560,950,20,20,":/images/block.png");
-    obstacle wooden_block3(300,1050,200,50,":/images/block.png"); //want to place coins above it
-    */
-
     // adding coins(diamonds)
     diamond.push_back(new diamonds(500,300));
     baselevel::adddiamond(diamond[0]);
