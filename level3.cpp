4f274a9d613d9ef56f3ae5336bc68ab67fecfee3
talia-mebed <talia@aucegypt.edu>
2024-12-07 00:11:32 +0200

    added objects

--- level3.cpp
+++ level3.cpp
@@ -26,6 +26,15 @@ void level3::initialize()
     obstacles.push_back(new obstacle(5000,0,1,1000,":/images/groundimage.png")); // right wall
     baselevel::addobstacle(obstacles[o++]);
     //adding ghasts
+
+    obstacles.push_back(new obstacle(400,420,400,100,":/images/woodenblock.png")); // block at 400,400 size 400/100
+    baselevel::addobstacle(obstacles[o++]);
+
+
+    enemies.push_back(new moving_enemy(500,320,600,400)); // spawns enemy at pos 500(x),280(y) and moves from 400-600x
+    baselevel::spawn_enemy(enemies[0]);
+
+
     ghasts.push_back(new ghast(1000,0));
     baselevel::addghast(ghasts[0]);
 
